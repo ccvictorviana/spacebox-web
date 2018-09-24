57430065073a884d02a00fddcbde47c5f7ac22cd
Infra <vjviana@thoughtworks.com>
2018-09-24 13:13:21 -0300

    Disable CORS

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/spacebox-web'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/spacebox-web/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/spacebox-web/index.html'));
 });
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+//     next();
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
