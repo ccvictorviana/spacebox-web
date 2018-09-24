9623096f24aa1224fb659b30428b2afbd7aa2937
Infra <vjviana@thoughtworks.com>
2018-09-24 13:34:16 -0300

    Remover Disble CORS

--- server.js
+++ server.js
@@ -11,18 +11,5 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/spacebox-web/index.html'));
 });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-//     next();
-// });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
