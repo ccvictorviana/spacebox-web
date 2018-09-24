0594dc65807cbeea3343e23c3ab2eb1c06107c1e
Infra <vjviana@thoughtworks.com>
2018-09-23 22:10:15 -0300

    Update config file to host Heroku

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  // authServiceHost: string = 'https://spacebox-auth-service.herokuapp.com';
-  // fileServiceHost: string = 'https://spacebox-file-service.herokuapp.com';
-  authServiceHost: string = 'http://localhost:7531';
-  fileServiceHost: string = 'http://localhost:8531';
+  authServiceHost: string = 'https://spacebox-auth-service.herokuapp.com';
+  fileServiceHost: string = 'https://spacebox-file-service.herokuapp.com';
+
+  // Config DEV
+  // authServiceHost: string = 'http://localhost:7531';
+  // fileServiceHost: string = 'http://localhost:8531';
 }
