bba56f9c518582096fb4bd22e6fc3ef6b43f68c5
Infra <vjviana@thoughtworks.com>
2018-10-30 19:29:41 -0300

    Update Global settings to Heroku

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,9 +1,9 @@
 export class Globals {
   // Config Heroku
-  // static AUTH_SERVICE_HOST: string = 'https://spacebox-auth-service.herokuapp.com';
-  // static FILE_SERVICE_HOST: string = 'https://spacebox-file-service.herokuapp.com';
+  static AUTH_SERVICE_HOST: string = 'https://spacebox-auth-service.herokuapp.com';
+  static FILE_SERVICE_HOST: string = 'https://spacebox-file-service.herokuapp.com';
 
   // Config Dev
-  static AUTH_SERVICE_HOST: string = 'http://localhost:7531';
-  static FILE_SERVICE_HOST: string = 'http://localhost:8531';
+  // static AUTH_SERVICE_HOST: string = 'http://localhost:7531';
+  // static FILE_SERVICE_HOST: string = 'http://localhost:8531';
 }
